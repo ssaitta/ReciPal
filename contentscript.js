2f8264d9395fff492ae52284b32cb52c52d07532
Sierra Saitta-Zelterman <ssaitta13@gmail.com>
2017-12-29 14:55:40 -0500

    Working with limited food library on food network, the framework to use on nytimes cooking and all recipes is built out

--- contentscript.js
+++ contentscript.js
@@ -1,27 +1,87 @@
-//this gives us a list of all the directions and ingredients
+"use strict"; 
 
-const [...directionsListHTML] = document.getElementsByClassName("o-Method__m-Body")
-const [...directionsList] = directionsListHTML[0].children
+//initalize the HTML class names we use to find the ingredients list and directions list
+let ingredientsClassName = ''
+let directionsClassName = ''
 
-//this will give us the list in the form of srings in an array of the directions
+//use the async API chrome.storage to retreive the url from the backgorund script. Then based on that url set the ingredients and directions class names for the three recipe sites, nytimes cooking, foodnetwork, and all recipes. 
 
-directionsList.forEach(elem=>{  
-    if(elem.childElementCount===0){
-        console.log(elem.innerText)
-        
+chrome.storage.sync.get('url', (obj)=>{   
+    console.log('fetched')
+    let url = obj.url
+    if(url.indexOf('nytimes')!==-1){
+        ingredientsClassName = 'recipeIngredient'
+        directionsClassName = 'recipeInstructions'
+        console.log(ingredientsClassName)
+    }
+    else if(url.indexOf('foodnetwork')!==-1){
+        ingredientsClassName = 'o-Ingredients__a-ListItemText'
+        directionsClassName = 'o-Method__m-Body'
+    }
+    else if(url.indexOf('allrecipes')!==-1){
+        ingredientsClassName = 'checkList'
+        directionsClassName = 'recipe-directions__list'
     }
 })
 
-const [...ingredientsList] = document.getElementsByClassName("o-Ingredients__a-ListItemText")
+console.log(ingredientsClassName)   //right now this is coming through as blank because chrome.storage.sync.get is async
 
+//currently hard coded to work with food network:
+
+//breaking down the ingredients list into the ingredients object:
+let [...ingredientsList] = document.getElementsByClassName("o-Ingredients__a-ListItemText") //"o-Ingredients__a-ListItemText"
+console.log(ingredientsList)
+let ingredientObj = {}
 //this will give us the list in the from of strings in an array of the ingredients 
 
-ingredientsList.forEach(elem=>{
-    //if(elem.childElementCount===0){
-        //elem.classList.add('highlightedIngred') //this works but css doesn't c
-        console.log(elem.innerHTML)
-    //}
+
+//building the ingredients object:
+ingredientsList.forEach((elem,index)=>{
+    let num = ['1','2','3','4','5','6','7','8','9','1/2','1/3','1/4','1/8','2/3','2/4','3/4']
+    let measurments = ['cup','cups','teaspoon','teaspoons','tablespoon','tablespoons','clove','cloves','medium','pound','pounds','sprig','sprigs','plus','about','small','pinch']
+    let food = ['onion','onions','garlic','oil','sausage','sage','wine','chicken','stock','pumpkin','heavy', 'cream' ,'salt','pepper','penne','romano','parmiigiano','pumpernickel','bay', 'leaf', 'cinnamon','nutmeg','cheese','spinach','apple','apples','vinegar','mustard','honey']
+    let removedCommans = elem.innerHTML.replace(',','');
+    let fullTextSplit = removedCommans.split(' ')
+    let quantityTextPart = fullTextSplit.filter((word)=>{
+       if(num.indexOf(word) !== -1 || measurments.indexOf(word) !== -1){
+           return word
+       }
+    }) 
+
+    let foodTextPart = fullTextSplit.filter((word)=>{
+        if(food.indexOf(word) !== -1){
+            return word
+        }
+    }) 
+    ingredientObj[index] = {}
+    ingredientObj[index]['quantity'] = quantityTextPart.join(' ')
+    ingredientObj[index]['foodPart'] = foodTextPart//.join(' ')
 })
+
+console.log(ingredientObj)
+
+
+//building out the directions list and making it iterable
+let [...directionsListHTML] = document.getElementsByClassName("o-Method__m-Body") //"o-Method__m-Body"
+let [...directionsList] = directionsListHTML[0].children
+
+//this will give us the list in the form of srings in an array of the directions
+
+
+//Using for loops to locate all ingredients in the directions section and adding the highlight and tooltip text.
+directionsList.forEach(elem=>{  
+    if(elem.childElementCount===0){
+        for(let i=0;i<Object.keys(ingredientObj).length;i++){
+            for(let j=0;j<ingredientObj[i].foodPart.length;j++){
+                if(elem.innerHTML.indexOf(ingredientObj[i].foodPart[j]!==-1)){
+                    console.log()
+                    elem.innerHTML = elem.innerHTML.replace(ingredientObj[i].foodPart[j], `<span style="background-color:yellow" title="`+ingredientObj[i].quantity+`">`+ingredientObj[i].foodPart[j]+`</span>`)
+                }
+            }
+        }
+    }
+})
+
 
 //testing on 
 //http://www.foodnetwork.com/recipes/rachael-ray/pasta-with-pumpkin-and-sausage-recipe-1939614
--- recipal.js
+++ recipal.js
@@ -1,21 +1,26 @@
-//document here is the popup.html not the page we're on
+//Utility function for getting the current url taken from the chrome getting started tutorial
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.query(queryInfo, (tabs) => {
+    var tab = tabs[0];
+    var url = tab.url;
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+    callback(url);
+  });
+}
+
+//whenever the browser action is clicked the chrome stored url is updated and the content script is run.
+chrome.browserAction.onClicked.addListener(function(tab,url) {
+  getCurrentTabUrl((url) => {
+    chrome.storage.sync.set({"url": url},()=>{
+    console.log('saved') 
+    console.log(url)
+   })
+  })
   chrome.tabs.executeScript(null, {file: "contentscript.js"});
 });
-
-// chrome.extension.getBackgroundPage().console.log('foo');
-
-document.addEventListener('DOMContentLoaded', () => {
-  getCurrentTabUrl((url) => {
-
-    console.log("nothing")
- 
-  });
-});
-
-
-
-
-
 
